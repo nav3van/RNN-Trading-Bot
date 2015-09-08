3c3a98da0e27c80335db859a3b14e6e4729db39c
[evanhenri] <[nonce@protonmail.ch]>
2015-09-08 17:48:57 -0400

    Updated output.sqlite path description

--- exchange.h
+++ exchange.h
@@ -17,16 +17,12 @@ public:
   Trade GetTrade(double current_price, double predicted_price);
   void log(enum Trade order_type, double predicted_price);
 private:
-  double usd_balance_;
-  double btc_balance_;
-  double purchase_price;
   double last_price_;
 
   int rc_;
   sqlite3 *output_db_;
-  const char* output_data_file_ = "/home/evan/hdd1/programming/projects/ann_trading_bot/data/output.sqlite";
-  //const char* output_data_file_ = "output.sqlite";
-  std::string output_data_table_ = "output";;
+  const char* output_data_file_ = "/home/evan/hdd1/programming/git_repos/neural-network-trading-bot/data/output.sqlite";	// change this to your local path to output.sqlite
+  std::string output_data_table_ = "output";
   char *z_err_msg_ = 0;
   const int output_db_timeout = 5000;
 };
