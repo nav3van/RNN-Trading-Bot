ea5e6fa210e3693481deb816330a3062f0d605f0
[evanhenri] <[nonce@protonmail.ch]>
2015-09-08 17:42:12 -0400

    Request trade data every 2 seconds

--- pyTrader/trade_stream.py
+++ pyTrader/trade_stream.py
@@ -1,77 +1,123 @@
-import urllib.request
-import urllib.parse
-import json
-import socket
-import codecs
-import sqlite3
-import time
-import sys
-
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'}
-reader = codecs.getreader("utf-8")
-
-url = 'https://btc-e.com/api/3/ticker/btc_usd'
-
-input_file = '/home/evan/hdd1/programming/projects/ann_trading_bot/data/input.sqlite'
-print(input_file)
-table_name = "price_data"
-
-db_timeout = 15
-socket_timeout = 15
-
-# Specifies how long a socket should wait for a response before timing out
-socket.setdefaulttimeout(socket_timeout)
-
-connect = sqlite3.connect(input_file, db_timeout)
-cursor = connect.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS " + table_name + " (vol DOUBLE, buy DOUBLE, sell DOUBLE, vol_cur DOUBLE, last DOUBLE)")
+import os, sys, time, datetime
+import urllib.request, urllib.parse, json, codecs, socket
+from file_check import parent_dir
 
 
-def create_comparison_json(buy, sell, last):
-    data = {'buy':buy, 'sell':sell, 'last':last}
-    return json.dumps(data)
+# Dynamically imports the class required for the extension returned by get_output_type
+# Returns the name of the class and the imported module containing that class
+def import_class_module():
+    module = 'sqlite_output'
+    temp_class_name = 'sqlite_obj'
+    temp_imported_class = __import__(module, fromlist=temp_class_name)
+    return temp_class_name, temp_imported_class
 
-# Allows first response to be inserted as there is no prior response to compare against
-initial_pass_flag = True
 
-# Contain JSON of prior API response to compare with current response to check for uniqueness
-prior = {}
+# Continue asking user for input if an entry that does not correlate to an index of _extension_list is entered
+# Return user entry once a valid input has been made
+def get_user_input(_extension_list):
+    while True:
+        try:
+            usr_input = int(input())
+        except ValueError:
+            print('***Invalid Input***\nEnter a number from 0 to ' + str(len(_extension_list) - 1) + ' inclusive')
+            continue
+        else:
+            if usr_input < 0 or usr_input >= len(_extension_list):
+                print('***Invalid Input***\nEnter a number from 0 to ' + str(len(_extension_list) - 1) + ' inclusive')
+            else:
+                return usr_input
 
-# Attempt HTTP request
-# If http request fails, attempt it up to 5 times in total, up to 4 additional attempts after initial request
-# If request fails after 5 attempts, execution does not continue
-while True:
-    attempts = 0
-    while attempts < 5:
+# Returns true if api response contains valid data and false otherwise
+def valid_response(_json_resp):
+    err_originator_desc = 'json response validation'
+    try:
+        if 'btc_usd' not in _json_resp:
+            return False
+    except ValueError as e:
+        store_log([err_originator_desc,e])
+    return True
+
+# Returns BTC-e API response with current exchange rates
+def http_request():
+    max_requests = 10
+
+    # Specifies how long a socket should wait for a response before timing out
+    socket.setdefaulttimeout(socket_timeout)
+
+    while max_requests > 0:
         try:
             api_request = urllib.request.Request(url, headers=headers)
             api_response = urllib.request.urlopen(api_request)
-            json_obj = json.load(reader(api_response))
-            break
-        except urllib.request.HTTPError as err:
-            attempts += 1
-            print(err.code)
-            print(err.read())
+            json_response = json.load(reader(api_response))
 
-    current = create_comparison_json(json_obj['btc_usd']['buy'], json_obj['btc_usd']['sell'], json_obj['btc_usd']['last'])
+            if valid_response(json_response):
+                return json_response
+            else:
+                store_log(['***Bad response***', str(json_response)])
+        except urllib.request.HTTPError as e:
+            print('e1')
+            store_log(['receiving api response', e])
+        except Exception as e:
+            print('e2')
+            store_log(['api request', e])
 
-    try:
-        if current != prior or initial_pass_flag:
-            cursor.execute("INSERT INTO " + table_name + " (vol, buy, sell, vol_cur, last) values(?,?,?,?,?)",
-                   (json_obj['btc_usd']['vol'], json_obj['btc_usd']['buy'], json_obj['btc_usd']['sell'],
-                    json_obj['btc_usd']['vol_cur'], json_obj['btc_usd']['last']))
-            connect.commit()
+        max_requests -= 1
+        time.sleep(2)
 
-            prior = create_comparison_json(json_obj['btc_usd']['buy'], json_obj['btc_usd']['sell'], json_obj['btc_usd']['last'])
-            initial_pass_flag = False
+    print('***API request threshold met with no valid responses received***')
 
-        cursor.execute("SELECT COUNT(*) FROM " + table_name)
-        record_count = cursor.fetchone()[0]
 
-        sys.stdout.write("\rTrade record count = %i" % record_count)
+def get_trade_data():
+    while True:
+        json_obj = http_request()
+
+        g_output_object.open_output_file()
+        timestamp = datetime.datetime.fromtimestamp(int(json_obj['btc_usd']['updated'])).strftime('%Y%m%d%H%M%S')
+        g_output_object.store_output(timestamp, json_obj['btc_usd']['sell'], json_obj['btc_usd']['buy'])
+
+        sys.stdout.write("\rTrade record count = %i" % g_output_object.record_count())
         sys.stdout.flush()
-    except sqlite3.ProgrammingError as e:
-        print(e)
+        time.sleep(2)
 
-    # BTC-e API updates every 2 seconds. Submitting requests more frequently results in duplicate responses
-    time.sleep(2)
+
+def store_log(_argv):
+    with open(parent_dir + 'data/error.log', 'a+') as log_file:
+        log_file.write('\n/-------------------\\\n')
+        for argc in _argv:
+            if argc == _argv[0]:
+                log_file.write('Error occurred during ' + argc + ' at ' + str(datetime.datetime.now()))
+            else:
+                log_file.write(str(argc))
+        log_file.write('\n\\-------------------/\n')
+    log_file.close()
+
+
+current_dir_oath = os.path.realpath(os.path.dirname(''))
+out_dir_path = parent_dir + 'data/'
+out_file_name = 'input.sqlite'
+
+socket_timeout = 15
+url = 'https://btc-e.com/api/3/ticker/btc_usd'
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17'}
+reader = codecs.getreader('utf-8')
+
+
+def main():
+    class_name, imported_class = import_class_module()
+    mod_attr = getattr(imported_class, class_name)
+    output_path = out_dir_path + out_file_name
+
+    global g_output_object
+    g_output_object = mod_attr(output_path)
+
+    # http_request() is wrapped in an infinite loop. If an exception is encountered during http_request(),
+    # the local infinite loop within that function will break. However, the infinite loop below will prevent
+    # execution from stopping.
+    while True:
+        get_trade_data()
+        store_log(['http_request() infinite loop', ['Pausing for 2 seconds before re-initiating request sequence']])
+        time.sleep(2)
+
+
+if __name__ == '__main__':
+    main()
